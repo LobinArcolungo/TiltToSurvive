7495bba0b10f8b8a27eea65c6f262fdd791ae79d
LobinArcolungo <giacomo.cero@gmail.com>
2025-01-13 14:44:21 +0100

    Acceleration early work, must be modified to comply with gravity

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734692789|9216991594';
+const CACHE_VERSION = '1736524459|9166986056';
 /** @type {string} */
 const CACHE_PREFIX = 'Tilt To Survive-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
