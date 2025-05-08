0f32b949cb90b4da19f833b054fcd5368b9fc12b
LobinArcolungo <giacomo.cero@gmail.com>
2025-05-08 22:17:04 +0200

    added background and predisposition for parallax effect

--- tilt_to_survive_project/player_area.gd
+++ tilt_to_survive_project/player_area.gd
@@ -1,13 +1,16 @@
 extends Node2D
 var start
 var size
-var radius =10
+var radius =15
 
 func _draw() -> void:
 	start = %Player.playerAreaStart
 	size = %Player.playerAreaSize
 	var style_box = StyleBoxFlat.new()
+	style_box.bg_color=Color(1,1,1,0.2)
+	style_box.border_color=Color(0,0,0)
 	style_box.set_corner_radius_all(radius)
+	style_box.set_border_width_all(1)
 	draw_style_box(style_box, size)
 	#draw_rect(size, Color.WHITE,false,4,true)
 
